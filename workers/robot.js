064f87d561649e03a46f8627837cd4ec81ebbe4e
RajatDatta5315 <rajatdatta90000@gmail.com>
2026-01-01 19:25:42 +0530

    Delete workers/robot.js

--- workers/robot.js
+++
@@ -1,8 +0,0 @@
-// DEAD ROBOT - DO NOT WAKE UP
-console.log("💀 NEHIRA VISION: TERMINATED.");
-
-// Ek empty loop jo kuch nahi karta, bas process zinda rakhta hai taaki crash na ho
-setInterval(() => {
-  // Sannata...
-}, 1000 * 60 * 60 * 24); // 24 Ghante mein ek baar saans lega
-
